265512303a8f021c295521cd0aecfd4b275406f2
JP Albeza <johnpaul.albeza@gmail.com>
2016-07-20 22:14:38 +1000

    Fix ci and add sample_search

--- icicle/api/ci/deploy_lambda.sh
+++ icicle/api/ci/deploy_lambda.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-LAMBDA_ARN=$1
+# Deployed to jpalbeza @ 6152-5215-1637
+
 SOURCE_DIR=`pwd`
-LAMBDA_NAME=IcicleSearch
-ZIP_FILE=${LAMBDA_NAME}.zip
-S3_BUCKET=s3://lambda.us-west-2.icicle.doobi.com
+LAMBDA_ARN=IcicleSearch
+ZIP_FILE=${LAMBDA_ARN}.zip
+S3_BUCKET=lambda.us-west-2.icicle.doobi.com
 
 # Operate in a temporary directory
 TMP_DIR=`mktemp -d`
@@ -22,7 +23,7 @@ rm -f $ZIP_FILE
 zip -r $ZIP_FILE *
 
 # Upload to S3
-aws s3 cp $ZIP_FILE $S3_BUCKET
+aws s3 cp $ZIP_FILE s3://$S3_BUCKET
 
 # Update the lambda with this new code
-aws lambda update-function-code --function-name $LAMBDA_ARN --s3-bucket $S3_BUCKET --s3-key $S3_BUCKET/$ZIP_FILE
+aws lambda update-function-code --function-name $LAMBDA_ARN --s3-bucket $S3_BUCKET --s3-key $ZIP_FILE
---
+++ icicle/api/test/sample_search.py
@@ -0,0 +1,10 @@
+import sys
+import urllib2
+from base64 import b64encode
+from pprint import pprint
+
+if __name__ == "__main__":
+    api_url = 'https://9u56icss4c.execute-api.us-west-2.amazonaws.com/dev'
+    keyword = b64encode(sys.argv[1])
+    content = urllib2.urlopen("{0}/search/{1}".format(api_url, keyword)).read()
+    pprint(content)
